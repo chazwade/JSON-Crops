0dbb2b1b3e0e24adc359707160d712ae46e42432
Cblair91 <admin@betadev.co.uk>
2016-04-15 17:23:09 +0100

    Textures + more methods for block

--- src/main/java/com/betadev/jsoncrops/block/BlockCrop.java
+++ src/main/java/com/betadev/jsoncrops/block/BlockCrop.java
@@ -1,22 +1,58 @@
 package com.betadev.jsoncrops.block;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 import com.betadev.jsoncrops.ModInfo;
 import com.betadev.jsoncrops.registry.ItemRegistry;
 import com.betadev.jsoncrops.tile.TileCrop;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.EnumPlantType;
 
 public class BlockCrop extends BlockCrops implements ITileEntityProvider {
+	@SideOnly(Side.CLIENT)
+	private IIcon[] icon = new IIcon[8];
+
 	public BlockCrop() {
 		super();
 		setBlockName(ModInfo.MOD_ID + ".crop");
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister ir) {
+		for(int i = 0; i < 8; i++) {
+			icon[i] = ir.registerIcon(ModInfo.MOD_ID + ":crop_base_" + i);
+		}
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta) {
+		return icon[meta];
+	}
+
+	@Override
+	public Item getItemDropped(int meta, Random random, int p_149650_3) {
+		return null;
+	}
+
+	@Override
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int meta, int fortune) {
+		return new ArrayList<ItemStack>();
 	}
 
 	@Override
